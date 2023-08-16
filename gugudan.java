7ff294e472c9702fc4dc26e1ce515d7b6f5ccde6
김도균 <wkmkyj991112@gmail.com>
2023-08-16 09:08:26 +0900

    ver.1

---
+++ gugudan.java
@@ -0,0 +1,17 @@
+
+
+class Gugudan{
+    public static void main(String[] args) {
+
+        int i, j;
+
+        for(i = 2; i < 10; i++){
+
+            for(j = 1; j < 10; j++){
+
+                System.out.println(i + " X " + j + " = " + i*j);
+            }
+        }
+    
+    }
+}
